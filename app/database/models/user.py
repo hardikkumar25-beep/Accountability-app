38d269e8514dc04d79d77ee3c830613858c7157d
hardikkumar25-beep <hardikkumar.work25@gmail.com>
2026-09-21 23:31:40 +0530

    added user model

---
+++ app/database/models/user.py
@@ -0,0 +1,23 @@
+from ..database import Base
+from sqlalchemy.orm import mapped_column,Mapped,relationship
+from sqlalchemy.sql import func
+from sqlalchemy import String, Column, Integer, Float,ForeignKey,DateTime,Boolean,Text
+from datetime import datetime
+
+class User(Base):
+    __tablename__="users"
+
+    id: Mapped[int]=mapped_column(primary_key=True)
+    username:Mapped[str]=mapped_column(String(50))
+    email:Mapped[str]=mapped_column(String(100),unique=True)
+    password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
+    created_at:Mapped[datetime]=mapped_column(DateTime,server_default=func.now())
+    profile: Mapped["UserProfile"] = relationship(back_populates="user",uselist=False)
+
+class UserProfile(Base):
+    __tablename__ = "user_profiles"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"),unique=True)
+    plan_description: Mapped[str | None] = mapped_column(Text)
+    user: Mapped["User"] = relationship(back_populates="profile")
